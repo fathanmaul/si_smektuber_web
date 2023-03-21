6d0a5646970284c59f28519a0037e7e91fed48ba
FlyingCat17 <fthnmrh012@gmail.com>
2023-03-21 22:03:45 +0700

    Downgrade laravel 8 FIX

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Log Into Your Account</title>
+  <link rel="shortcut icon" href="{{ asset('assets/favicon/smk-logo.png') }}" type="image/x-icon">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+  <div class="">
+
+  </div>
+
+</body>
+<script src="{{ asset('js/app.js') }}"></script>
+
+</html>
