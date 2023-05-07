ac3c03d6982429549cdd4b889d11cda4470983a9
Andini <94980101+andin01@users.noreply.github.com>
2023-05-07 16:38:07 +0700

    mengubah header

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,11 +1,8 @@
 <!-- Footer Start -->
-<div
-      class="container-fluid bg-light footer mt-5 pt-5 wow fadeIn"
-      data-wow-delay="0.1s"
-    >
-      <div class="container py-5">
+<div class="container-fluid bg-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
         <div class="row g-5">
-          <!-- <div class="col-md-6">
+            <!-- <div class="col-md-6">
             <h1 class="text-primary mb-4">
               <img
                 class="img-fluid me-2"
@@ -21,7 +18,7 @@
               lorem et sit.</span
             >
           </div> -->
-          <!-- <div class="col-md-6">
+            <!-- <div class="col-md-6">
             <h5 class="mb-4">Newsletter</h5>
             <p>Clita erat ipsum et lorem et sit, sed stet lorem sit clita.</p>
             <div class="position-relative">
@@ -38,10 +35,10 @@
               </button>
             </div>
           </div> -->
-          <!--           
+            <!--
             <div class="row g-5"> -->
-          <div class="col-md-4 pt-5 justify-content-center">
-            <!-- <h1 class="text-primary mb-4">
+            <div class="col-md-4 pt-5 justify-content-center">
+                <!-- <h1 class="text-primary mb-4">
                   <img
                     class="img-fluid me-2"
                     src="img/Logo_smektuber.png"
@@ -49,66 +46,58 @@
                     style="width: 100px"
                   />
                 </h1> -->
-            <img src="img/Logo_smektuber.png" alt="" />
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <h5 class="mb-4">Tentang Kami</h5>
-            <p>
-              SMKN 7 JEMBER adalah Sekolah Menengah Kejuruan yang terletak di
-              jalan PB.Sudirman No 16 Jatiroto, jember
-            </p>
-            <p>
-              <i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA
-            </p>
-            <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-            <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
-          </div>
-          <!-- <div class="col-lg-3 col-md-6">
+                <img src="img/Logo_smektuber.png" alt="" />
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h5 class="mb-4">Tentang Kami</h5>
+                <p>
+                    SMKN 7 JEMBER adalah Sekolah Menengah Kejuruan yang terletak di
+                    jalan PB.Sudirman No 16 Jatiroto, jember
+                </p>
+                <p>
+                    <i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA
+                </p>
+                <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
+            </div>
+            <!-- <div class="col-lg-3 col-md-6">
                     <h5 class="mb-4">Our Services</h5>
                     <a class="btn btn-link" href="">Currency Wallet</a>
                     <a class="btn btn-link" href="">Currency Transaction</a>
                     <a class="btn btn-link" href="">Bitcoin Investment</a>
                     <a class="btn btn-link" href="">Token Sale</a>
                 </div> -->
-          <!-- <div class="col-lg-3 col-md-6">
+            <!-- <div class="col-lg-3 col-md-6">
             <h5 class="mb-4">Quick Links</h5>
             <a class="btn btn-link" href="">About Us</a>
             <a class="btn btn-link" href="">Contact Us</a>
             <a class="btn btn-link" href="">Our Services</a>
             <a class="btn btn-link" href="">Terms & Condition</a>
           </div> -->
-          <div class="col-lg-3 col-md-6">
-            <h5 class="mb-4">Ikuti Kami</h5>
-            <div class="d-flex">
-              <a class="btn btn-square rounded-circle me-1" href=""
-                ><i class="fab fa-twitter"></i
-              ></a>
-              <a class="btn btn-square rounded-circle me-1" href=""
-                ><i class="fab fa-facebook-f"></i
-              ></a>
-              <a class="btn btn-square rounded-circle me-1" href=""
-                ><i class="fab fa-youtube"></i
-              ></a>
-              <a class="btn btn-square rounded-circle me-1" href=""
-                ><i class="fab fa-linkedin-in"></i
-              ></a>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="mb-4">Ikuti Kami</h5>
+                <div class="d-flex">
+                    <a class="btn btn-square rounded-circle me-1" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square rounded-circle me-1" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square rounded-circle me-1" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-square rounded-circle me-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="container-fluid copyright">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-              &copy; <a href="#">JTI POLIJE NekoID 2023</a>, All Right Reserved.
-            </div>
-            <div class="col-md-6 text-center text-md-end">
-              <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-              Version
-              <a href="https://google.com">1.0.0</a>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-    <!-- Footer End -->
+    <div class="container-fluid copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a href="#">JTI POLIJE NekoID 2023</a>, All Right Reserved.
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                    Version
+                    <a href="https://google.com">1.0.0</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
