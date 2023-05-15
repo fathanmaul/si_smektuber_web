7a7aec30230c89709037203f62ae9201afdf97db
Andini <andinip2002@gmail.com>
2023-05-16 00:11:10 +0700

    penambahan detail

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EkstrakurikulerController;
 use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PpdbController;
 use App\Http\Controllers\PrestasiController;
+use App\Http\Controllers\PrestasiEkstrakurikulerController;
+use App\Http\Controllers\PrestasiJurusanController;
 use App\Http\Controllers\SekolahController;
+use App\Http\Controllers\TahunAjaranController;
 use App\Models\About;
 use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
@@ -48,14 +52,17 @@ Route::get('/konsultasi', function () {
     return view('landing.konsultasi');
 });
 
-Route::get('/ekstra', function () {
-    return view('landing.detail.ekstra');
-});
-
 Route::get('/jurusan', function () {
     return view('landing.detail.jurusan');
 });
 
+Route::get('/ekstra', function () {
+    return view('landing.detail.ekstra');
+});
+
+Route::get('/blog', function () {
+    return view('landing.detail.blog');
+});
 /**
  * 
  * 
@@ -113,22 +120,32 @@ Route::prefix('admin')->middleware(['middleware' => 'auth'])->group(function () 
         Route::post('/{id}/edit', [JurusanController::class, 'put'])->name('put');
         Route::delete('/{id}', [JurusanController::class, 'destroy'])->name('destroy');
 
+        Route::group(['prefix' => 'prestasi', 'as' => 'prestasi.'], function () {
+            Route::get('/', [PrestasiJurusanController::class, 'index'])->name('index');
+        });
     });
-    Route::group(['prefix' => 'prestasi'], function () {
-        Route::get('/', [PrestasiController::class, 'index'])->name('prestasi.index');
-        Route::get('/tambah', [PrestasiController::class, 'create'])->name('prestasi.create');
-        Route::post('/store', [PrestasiController::class, 'store'])->name('prestasi.store');
-        Route::get('/{id}/edit', [PrestasiController::class, 'edit'])->name('prestasi.edit');
-        Route::post('/{id}/edit', [PrestasiController::class, 'put'])->name('prestasi.put');
-        Route::delete('/{id}', [PrestasiController::class, 'destroy'])->name('prestasi.destroy');
+    // Route::group(['prefix' => 'prestasi'], function () {
+    //     Route::get('/', [PrestasiController::class, 'index'])->name('prestasi.index');
+    //     Route::get('/tambah', [PrestasiController::class, 'create'])->name('prestasi.create');
+    //     Route::post('/store', [PrestasiController::class, 'store'])->name('prestasi.store');
+    //     Route::get('/{id}/edit', [PrestasiController::class, 'edit'])->name('prestasi.edit');
+    //     Route::post('/{id}/edit', [PrestasiController::class, 'put'])->name('prestasi.put');
+    //     Route::delete('/{id}', [PrestasiController::class, 'destroy'])->name('prestasi.destroy');
+
+    // });
+    Route::group(['prefix' => 'ekstrakurikuler', 'as' => 'ekstrakurikuler.'], function () {
+        Route::get('/', [EkstrakurikulerController::class, 'index'])->name('index');
+        Route::get('/tambah', [EkstrakurikulerController::class, 'create'])->name('create');
+        Route::post('/tambah', [EkstrakurikulerController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [EkstrakurikulerController::class, 'edit'])->name('edit');
+        Route::post('/{id}/edit', [EkstrakurikulerController::class, 'put'])->name('put');
+        Route::delete('/{id}', [EkstrakurikulerController::class, 'destroy'])->name('destroy');
+
+        Route::group(['prefix' => 'prestasi', 'as' => 'prestasi.'], function () {
+            Route::get('/', [PrestasiEkstrakurikulerController::class, 'index'])->name('index');
+        });
     });
 
-<<<<<<< Updated upstream
-    Route::group(['prefix' => 'ekstrakurikuler'], function () {
-        Route::get('/', [EkstrakurikulerController::class, 'index'])->name('ekstrakurikuler.index');
-        Route::get('/tambah', [EkstrakurikulerController::class, 'create'])->name('ekstrakurikuler.create');
-        Route::delete('/{id}', [EkstrakurikulerController::class, 'destroy'])->name('ekstrakurikuler.destroy');
-=======
     Route::group(['prefix' => 'ppdb', 'as' => 'ppdb.'], function () {
         Route::group(['prefix' => 'tahun-ajaran', 'as' => 'tahun_ajaran.'], function () {
             Route::get('/', [TahunAjaranController::class, 'index'])->name('index');
@@ -142,7 +159,6 @@ Route::prefix('admin')->middleware(['middleware' => 'auth'])->group(function () 
             Route::get('/', [PpdbController::class, 'index'])->name('index');
             Route::get('/tambah', [PpdbController::class, 'create'])->name('create');
         });
->>>>>>> Stashed changes
     });
 });
 
