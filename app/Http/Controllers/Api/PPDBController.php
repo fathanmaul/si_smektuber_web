d97d1041e14097ccd3c8d770c12f65f034c49a9f
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-05-17 13:42:07 +0700

    fixed : models detailRegistration, and Api/ppdb

--- app/Http/Controllers/Api/PPDBController.php
+++ app/Http/Controllers/Api/PPDBController.php
@@ -15,58 +15,62 @@ class PPDBController extends Controller
     
     public function create(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'nisn' => 'required|string',
-            'full_name' => 'required|string',
-            'date_birth' => 'required|date',
-            'address' => 'required|string',
-            'phone' => 'required|string',
-            'father_name' => 'required|string',
-            'mother_name' => 'required|string',
-            'school_origin' => 'required|string',
-            'graduation_year' => 'required|date',
-            'major_id_1' => [
-                'required',
-                'integer',
-                Rule::exists('major', 'id')
-            ],
-            'major_id_2' => [
-                'required',
-                'integer',
-                Rule::exists('major', 'id')
-            ],
-        ]);
-
-        if ($validator->fails()) {
-            return Response::error('Validation Error', $validator->errors(), 422);
-        }
-
-        $user = auth()->user();
-
-        // Check if user_id is already registered
-        $isRegistered = Registration::where('user_id', $user->id)->exists();
-        if ($isRegistered) {
-            return Response::error('User is already registered', [], 400);
-        }
-        // Create a new registration for student
-        // $registration = new DetailRegistration();
-        // Create a new registration for student
-
-        $data= DetailRegistration::create([
-            'user_id' => $user->id,
-            'nisn' => $request->nisn,
-            'full_name' => $request->full_name,
-            'date_birth' => $request->date_birth,
-            'address' => $request->address,
-            'phone' => $request->phone,
-            'father_name' => $request->father_name,
-            'mother_name' => $request->mother_name,
-            'school_origin' => $request->school_origin,
-            'graduation_year' => $request->graduation_year,
-            'major_id_1' => $request->major_id_1,
-            'major_id_2' => $request->major_id_2,
-        ]);
+        try{
+            $validator = Validator::make($request->all(), [
+                'nisn' => 'required|string',
+                'full_name' => 'required|string',
+                'date_birth' => 'required|date',
+                'address' => 'required|string',
+                'phone' => 'required|string',
+                'father_name' => 'required|string',
+                'mother_name' => 'required|string',
+                'school_origin' => 'required|string',
+                'graduation_year' => 'required|date',
+                'major_id_1' => [
+                    'required',
+                    'integer',
+                    Rule::exists('major', 'id')
+                ],
+                'major_id_2' => [
+                    'required',
+                    'integer',
+                    Rule::exists('major', 'id')
+                ],
+            ]);
     
-        return Response::success($data, 'Registration successful', 200);
+            if ($validator->fails()) {
+                return Response::error('Validation Error', $validator->errors(), 422);
+            }
+    
+            $user = auth()->user();
+    
+            // Check if user_id is already registered
+            $isRegistered = Registration::where('user_id', $user->id)->exists();
+            if ($isRegistered) {
+                return Response::error('User is already registered', [], 400);
+            }
+            
+    
+            $data= DetailRegistration::create([
+                'user_id' => $user->id,
+                'nisn' => $request->nisn,
+                'full_name' => $request->full_name,
+                'date_birth' => $request->date_birth,
+                'address' => $request->address,
+                'phone' => $request->phone,
+                'father_name' => $request->father_name,
+                'mother_name' => $request->mother_name,
+                'guardian_name'=>$request->guardian_name,
+                'school_origin' => $request->school_origin,
+                'graduation_year' => $request->graduation_year,
+                'major_id_1' => $request->major_id_1,
+                'major_id_2' => $request->major_id_2,
+            ]);
+        
+            return Response::success($data, 'Registration successful', 200);
+        } catch (\Exception $e) {
+            return Response::error('internal server error', [], 500);
+        }
+    
     }
 }
